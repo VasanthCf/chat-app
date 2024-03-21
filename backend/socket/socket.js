3e7e23a626faf59f46a54973225f391d7d0fc783
VasanthCf <vegamvasanth11@gmail.com>
2024-03-21 21:17:43 +0530

    production

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const userSocketMap = {};
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3001"],
+    origin: ["http://localhost:5000"],
     methods: ["GET", "POST"],
   },
 });
