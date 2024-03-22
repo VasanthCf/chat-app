be3de8c3ae3af417aca0aa6476c59cdbb238cc47
VasanthCf <vegamvasanth11@gmail.com>
2024-03-22 19:36:10 +0530

    bug found cleared

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -8,13 +8,13 @@ const protectRoute = async (req, res, next) => {
 
     const token = req.cookies.jwt;
     if (!token) {
-      return res.send(401).json({ error: "Unauthorized User" });
+      return res.status(401).json({ error: "Unauthorized User" });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     if (!decoded) {
-      return res.send(401).json({ error: "Unauthorized Invalid token" });
+      return res.status(401).json({ error: "Unauthorized Invalid token" });
     }
 
     const user = await User.findById(decoded.userId).select("-password");
